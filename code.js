8c79d9de8516a5b45ff01701fced826acc125490
zakaria <zakaria@ymail.com>
2022-12-10 00:49:44 +0200

    arr

--- code.js
+++ code.js
@@ -15,7 +15,7 @@ hamburger.addEventListener("click", () => {
 document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 200) {
-    header.style.backgroundColor = "#29323c";
+    header.style.backgroundColor = "#29383c";
   } else {
     header.style.backgroundColor = "transparent";
   }
